bd1358111650d778452011193d8685572a183c2c
TayyibFZ <tayyibfayyaz@gmail.com>
2018-07-20 09:06:59 -0700

    alpha

--- Centurion/ViewController.swift
+++ Centurion/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import Foundation
+import Moscapsule
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var dataHub: UIButton!
+    @IBOutlet weak var backCam: UIButton!
+    @IBOutlet weak var sway: UIButton!
+    @IBAction func dataHub(_ sender: Any) {
+        self.performSegue(withIdentifier: "dataHubSegue" , sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dataHub.layer.cornerRadius = dataHub.frame.size.height/2
+        dataHub.layer.masksToBounds = true
+        dataHub.setGradientBackground(colorOne: Colors.blue, colorTwo: Colors.white)
+        
+        
+        sway.layer.cornerRadius = sway.frame.size.height/2
+        sway.layer.masksToBounds = true
+        sway.setGradientBackground(colorOne: Colors.orange, colorTwo: Colors.red)
+        
+        backCam.layer.cornerRadius = backCam.frame.size.height/2
+        backCam.layer.masksToBounds = true
+        backCam.setGradientBackground(colorOne: Colors.orange, colorTwo: Colors.red)
+        
+        view.setGradientBackground(colorOne: Colors.green, colorTwo: Colors.blue)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
 }
-
---
+++ Centurion/dataHubView.swift
@@ -0,0 +1,105 @@
+//
+//  dataHubView.swift
+//  Centurion
+//
+//  Created by Tayyib Fayyaz on 5/13/18.
+//  Copyright © 2018 Tayyib Fayyaz. All rights reserved.
+//
+
+import UIKit
+import Moscapsule
+import Foundation
+
+let mqttConfig = MQTTConfig(clientId: "iOS", host: "192.168.1.2", port: 1883, keepAlive: 9999)
+let mqttClient = MQTT.newConnection(mqttConfig)
+
+
+class dataHubView: UIViewController{
+    
+    @IBOutlet weak var humidityL: UILabel!
+    @IBOutlet weak var waterL: UILabel!
+    @IBOutlet weak var tempL: UILabel!
+    @IBOutlet weak var refreshM: UIButton!
+    @IBAction func backMain(_ sender: Any) {
+        self.performSegue(withIdentifier: "backMainSegue" , sender: self)
+        
+    }
+    @IBOutlet weak var backMain: UIButton!
+    @IBAction func dataSwitch(_ sender: UISwitch) {
+        if( sender.isOn == true){
+            mqttClient.connectTo(host: "192.168.1.2", port: 1883, keepAlive: 60)
+            mqttClient.subscribe("return/temp", qos: 2)
+            print("temp suscribed")
+            mqttClient.subscribe("return/water", qos: 2)
+            print("water suscribed")
+            mqttClient.subscribe("return/humid", qos: 2)
+            print("humid suscribed")
+            //mqttClient.connectTo(host: "10.0.0.40", port: 1883, keepAlive: 60)
+        } else {
+            mqttClient.disconnect()
+        }
+    }
+   /* @IBAction func refreshM(_ sender: Any) {
+         DispatchQueue.main.async {        print("TEST!")
+        mqttClient.connectTo(host: "172.20.10.5", port: 1883, keepAlive: 60)
+        mqttClient.subscribe("return/temp", qos: 2)
+        print("temp suscribed")
+        mqttClient.subscribe("return/water", qos: 2)
+        print("water suscribed")
+        mqttClient.subscribe("return/humid", qos: 2)
+        print("humid suscribed")
+        //mqttClient.connectTo(host: "10.0.0.40", port: 1883, keepAlive: 60)
+        }
+    } */
+    
+    
+    
+    override func viewDidLoad() {
+        mqttConfig.onConnectCallback = { returnCode in
+            // DispatchQueue.main.async {
+            if returnCode == ReturnCode.success {
+                print("Connected!")
+            }
+            else {
+                print("Error")
+            }
+        }
+        // }
+        mqttConfig.onMessageCallback = { mqttMessage in
+            DispatchQueue.main.async {
+                let deku = mqttMessage.payloadString
+                if mqttMessage.topic == "return/temp" {
+                    print("temp")
+                    self.tempL.text = deku
+                } else if mqttMessage.topic == "return/water"{
+                    print("water")
+                    self.waterL.text = deku
+                } else if mqttMessage.topic == "return/humid"{
+                    print("humid")
+                    self.humidityL.text = deku
+                }
+            }
+        }
+        super.viewDidLoad()
+     
+            backMain.layer.cornerRadius = backMain.frame.size.height/2
+            backMain.layer.masksToBounds = true
+            backMain.setGradientBackground(colorOne: Colors.white, colorTwo: Colors.blue)
+           /* refreshM.layer.cornerRadius = backMain.frame.size.height/2
+            refreshM.layer.masksToBounds = true
+            refreshM.setGradientBackground(colorOne: Colors.red, colorTwo: Colors.veryDarkGrey)*/
+            view.setGradientBackground(colorOne: Colors.blue, colorTwo: Colors.green)
+        // Do any additional setup after loading the view, typically from a nib.
+      
+ 
+    
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+
+
